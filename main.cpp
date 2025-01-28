7dd243920778c20f757bcbc8fe58b6b3974b3bf1
NangueAnges <nangueanges@gmail.com>
2025-01-28 11:41:13 +0100

    documentation du code

--- main.cpp
+++ main.cpp
@@ -17,45 +17,56 @@ void Push(){
     std::cout << "Quel type d'element souhaitez-vous utiliser? (int, float, string) \n";
     std::cin >> type;
 
-    Element element; // Crée un nouveau élément
-    element.type = type;
+//creation d'un nouvel element de la pile
+    Element element; 
+    element.type = type;//stocke le type choisi par l'utilisateur (int, float ou string)
 
     if (type == "int"){
-        int val;
+        int val;//variable pour stocker un entier temporairement
         std::cout << "Entrer un entier au choix \n";
-        std::cin >> val;
-        element.value = std::to_string(val); // Convertir l'entier en string
-    } else if (type == "float"){
-        float val;
+        std::cin >> val;//saisir de l'entier par l'utilisateur
+        element.value = std::to_string(val); // Conversion de l'entier en string(chaine de caracteres)
+    } 
+    else if (type == "float"){
+        float val;//variable pour stocker un flottant temporairement
         std::cout << "Entrer un flottant au choix \n";
-        std::cin >> val;
-        element.value = std::to_string(val); // Convertir le float en string
-    } else if(type == "string"){
-        std::string val;
+        std::cin >> val;//saisir du flottant par l'utilisateur
+        element.value = std::to_string(val); // Conversion du flottant en string(chaine de caractere)
+    } 
+    else if(type == "string"){
+        std::string val;//variable pour stocker une chaine de caractere temporairement
         std::cout << "Entrer une chaine de caractères au choix \n";
-        std::cin.ignore(); // Ignorer le retour à la ligne
-        std::getline(std::cin, val);
-        element.value = val;
-    } else {
+        std::cin.ignore(); // Ignorer le caractere de retour à la ligne
+        std::getline(std::cin, val);//recupere toute la ligne de texte saisir par l'utilisateur
+        element.value = val;//stocke directement la chaine de caractere
+    } 
+    else {
+        //affiche ce message au cas ou le type entrer par l'utilisateur est invalide
         std::cout << "Type invalide !!!! \n";
-        return; 
+        return; //sort de la fonction sans ajouter l'element
     }
+//ajoute l'element dans la pile
     pile.push_back(element);
-    std::cout << "L'element ajoute à la pile ! \n";
+    std::cout << "L'element est ajoute à la pile ! \n";
 }
+
+
 
 // Fonction pour retirer un élément à la fin de la pile
 void Pop(){
-    if (pile.empty()){
+    if (pile.empty()){//verifie si la pile est vide
         std::cout << "La pile est vide... aucun element à retirer !!! \n";
     } else {
-        Element element = pile.back(); // Accède directement au dernier élément de la pile  
+        Element element = pile.back(); // Accède directement au dernier élément de la pile et le recupere  
         pile.pop_back(); // Supprime le dernier élément de la pile
         std::cout << "Le dernier element de la pile retire est : ";
-        if (element.type == "int"){
-            std::cout << stoi(element.value) << "(int)\n";//passe du string a integer (string to integer)
+        if (element.type == "int"){//verifie si le dernier element est un entier
+            std::cout << stoi(element.value) << "(int)\n";//passe la chaine de caracteres contenant 
+                                                          //un nombre entier en une variable de 
+                                                          //type int ( "42" devient 42 )
         } else if (element.type == "float"){
             std::cout << stof(element.value) << "(float)\n";//(string to float)
+                                                            //pareillement, mais avec les flottants ( "3,4" devient 3,4 )
         } else if (element.type == "string"){
             std::cout << element.value << "(string)\n";
         }
@@ -68,49 +79,55 @@ void enqueueFile(){
     std::cout << "Quel type de valeurs souhaitez-vous manipuler ?(int, float, string) \n";
     std::cin >> type;
 
-    Element element; // Nouveau élément
-    element.type = type;
+//creation d'un nouvel element de la file
+    Element element; 
+    element.type = type;//stocke le type d'element choisi par l'utilisateur
     if (type == "int"){
-        int val;
+        int val;//variable pour stocker temporairement un entier 
         std::cout << "Entrer un entier \n";
-        std::cin >> val;
-        element.value = std::to_string(val);
+        std::cin >> val;//recupere l'element saisi par l'utilisateur
+        element.value = std::to_string(val);//convertir l'entier en chaine de caractere
     } else if (type == "float"){
-        float val;
+        float val;//variable pour stocker un flottant te;porairement
         std::cout << "Entrer un flottant au choix \n";
-        std::cin >> val;
-        element.value = std::to_string(val);
+        std::cin >> val;//recupere le flottant saisi par l'utilisateur
+        element.value = std::to_string(val);//convertir le flottant en chaine de caractere
     } else if (type == "string"){
-        std::string val;
+        std::string val;//variable pour stocker temporairement une chaine de caractere 
         std::cout << "Entrer une chaine de caracteres \n";
-        std::cin.ignore(); // Pour ignorer le retour à la ligne
-        std::getline(std::cin, val);
+        std::cin.ignore(); //ignorer le caractere de retour à la ligne
+        std::getline(std::cin, val);//recupere toute la ligne de texte saisir par l'utilisateur
         element.value = val;
     } else {
+//affiche ce message au cas ou l'utilisateur ne fait aucun choix valide
         std::cout << "Type invalide !!! \n";
-        return;
+        return;//sort de la fonction sans ajouter l'element
     }
+//ajoute l'element a la fin de la file
     file.push_back(element);
     std::cout << "L'element est ajouté \n";
 }
 
+
 // Fonction pour retirer un élément au début de la file
 void dequeueFile (){
-    if (file.empty()){
+    if (file.empty()){//verifie si la file est vide
         std::cout << "La file est vide \n";
     } else {
         Element first = file.front(); // Identifie et récupère le premier élément de la file
-        file.erase(file.begin()); // Supprime cet élément
+        file.erase(file.begin()); // Supprime cet élément de la file
         std::cout << "Le premier element retire est : ";
+//verifie le type du premier element de la file
         if (first.type == "int"){
-            std::cout << stoi(first.value) << "(int)\n";
+            std::cout << stoi(first.value) << "(int)\n";//(permet de quitter de "42" a 42)
         } else if (first.type == "float"){
-            std::cout << stof(first.value) << "(float)\n";
+            std::cout << stof(first.value) << "(float)\n";//(permet de quitter de "42.5" a 42.5)
         } else if (first.type == "string"){
             std::cout << first.value << "(string)\n";
         }
     }
 }
+
 
 // Fonction permettant d'afficher les éléments contenus dans la pile
 void afficheEtatP(){
@@ -119,12 +136,13 @@ void afficheEtatP(){
     } else {
         std::cout << "Le contenu de la pile est : \n";
         std::cout << "Pile = [ Top = ";//ouvre l'affichage de la pile
-        for(int i = (pile.size() - 1); i >= 0; i--) { // Parcours la pile du sommet à la base et affiche l'element sans la virgule a le fin
-        //std::cout <<pile[i].value<<" ";
-            if (i != (pile.size() - 1)){
+        for(int i = (pile.size() - 1); i >= 0; i--) { // Parcours la pile du sommet à 
+                                                      //la base et affiche l'element sans la virgule a le fin
+            if (i != (pile.size() - 1)){//verifie que l'element n'est pas le dernier 
                 std::cout <<" , ";//ajoute une virgule si ce n'est pas le dernier element
             }
-        std::cout <<pile[i].value<<" ";
+        std::cout <<pile[i].value<<" ";//ajoute la valeur de l'element saisir par l'utilisateur
+//ajoute le type d'element entrer par l'utilisateur en plus de sa valeur(34 (int)) 
             if (pile[i].type == "int"){
                 std::cout << "(int)";
             } else if (pile[i].type == "float"){
@@ -137,6 +155,7 @@ void afficheEtatP(){
     }
 }
 
+
 // Fonction permettant d'afficher les éléments de la file
 void afficheEtatF(){
     if (file.empty()){//verifie si la file est vide
@@ -145,12 +164,13 @@ void afficheEtatF(){
         std::cout << "Le contenu de la file est : \n";
         std::cout << "File = [ Front = ";//ouvre l'affichage de la file
         for(size_t i = 0; i < file.size(); i++) { // size_t est un type entier non signé
-                                                  //parcours la file du debut a la fin et affiche l'element sans virgule a la fin
-        //std::cout << file[i].value << " ";
-            if (i != 0){
+                                                  //parcours la file du debut a la fin et
+                                                  //affiche l'element sans virgule a la fin
+            if (i != 0){//verifie s'il ne s'agit pas du premier element
             std::cout << " , ";//affiche une virgule sauf avant le premier element
             }
-        std::cout << file[i].value << " ";
+        std::cout << file[i].value << " ";//ajoute la valeur de l'entier saisir par l'utilisateur
+//verifie le type de l'element et l'affiche 
             if (file[i].type == "int"){
                 std::cout << "(int)";
             } else if (file[i].type == "float"){
@@ -162,6 +182,7 @@ void afficheEtatF(){
         std::cout << "]\n";//ferme l'affichage de la file 
     }
 }
+
 
 // Fonction principale du programme
 int main(int argc, char** argv) {
